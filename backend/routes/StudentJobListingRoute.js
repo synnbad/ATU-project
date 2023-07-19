346868d77f96b05aad1f73f7f9acdcbb28ab9b17
Sinbad Adjuik <saajuik@gmail.com>
2023-07-19 12:35:35 -0700

    changes

--- backend/routes/StudentJobListingRoute.js
+++ backend/routes/StudentJobListingRoute.js
@@ -1,12 +1,17 @@
-// Student Job Listing route
-app.get("/StudentJobListing", async (req, res) => {
-    try {
-      // Fetch all job listings
-      const jobListings = await JobListing.find();
-  
-      res.status(200).json(jobListings);
-    } catch (error) {
-      console.error("Error fetching job listings:", error);
-      res.status(500).json({ error: "An error occurred while fetching job listings" });
-    }
-  });
+import express from "express";
+import JobListing from "../backend/Models/JobListing.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const jobListings = await JobListing.find();
+
+    res.status(200).json(jobListings);
+  } catch (error) {
+    console.error("Error fetching job listings:", error);
+    res.status(500).json({ error: "An error occurred while fetching job listings" });
+  }
+});
+
+export default router;
